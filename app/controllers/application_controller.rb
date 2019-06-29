c73297d8ecd27182edab2403f9fdca67f1224bc0
Yingqi Chen <chanwk18@hotmail.com>
2019-06-29 18:55:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,11 @@ class App < Sinatra::Base
 
     post '/team' do
 
-
-    erb :team
+      @team=Team.new( params[:team][:name],params[:team][:motto])
+      @hero_1=Hero.new(params[:member1_name],params[:member1_power],params[:member1_bio])
+      @hero_2=Hero.new(params[:member2_name],params[:member2_power],params[:member2_bio])
+      @hero_3=Hero.new(params[:member3_name],params[:member3_power],params[:member3_bio])
+      erb :team
 
     end
 
